138073e8c3545bb1cdaa93eba4f0f17c8d1ed5de
BRANDON LUGO COSTILLA <I20050022@monclova.tecnm.mx>
2020-09-11 18:32:17 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola no entiendo bien el visual studio y el github");
+            Console.ReadKey();
+        }
+    }
+}
